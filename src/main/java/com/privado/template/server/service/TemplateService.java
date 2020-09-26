9be0477128bb30908e092cb8ebc8ce7e4e329d1d
denniskbijo <dennismartin2012@gmail.com>
2020-09-26 09:20:40 +0530

    Modified getTemplateByCustomerId to throw DocumentNotFoundException

--- src/main/java/com/privado/template/server/service/TemplateService.java
+++ src/main/java/com/privado/template/server/service/TemplateService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.privado.template.server.bean.Template;
+import com.privado.template.server.exception.DocumentNotFoundException;
 import com.privado.template.server.repo.TemplateRepository;
 
 /**
@@ -36,8 +37,16 @@ public class TemplateService {
 	 * 
 	 * @param customerId
 	 * @return
+	 * @throws DocumentNotFoundException
 	 */
-	public Template getTemplateByCustomerId(String customerId) {
-		return repo.findByCustomerId(customerId);
+	public Template getTemplateByCustomerId(String customerId) throws DocumentNotFoundException {
+
+		List<Template> resultTemplates = repo.findByCustomerId(customerId);
+		if (!resultTemplates.isEmpty())
+			return resultTemplates.get(0);
+		else
+			throw new DocumentNotFoundException("No Template found for customer: " + customerId);
 	}
+
+
 }
